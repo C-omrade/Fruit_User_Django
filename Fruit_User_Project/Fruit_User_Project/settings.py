7aa064c6dba13241981c3de55e6e5de25902e873
C-omrade <alone.silent.hunter@gmail.com>
2025-01-16 22:44:18 +0530

    Created_model

--- Fruit_User_Project/Fruit_User_Project/settings.py
+++ Fruit_User_Project/Fruit_User_Project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Fruits',
+    'Users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
